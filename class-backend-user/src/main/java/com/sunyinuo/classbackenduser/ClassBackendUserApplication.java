2f772cad9b322a1aff10f216b1379e3a3cdcee78
孙一诺 <11404164+sun-yi-nuo@user.noreply.gitee.com>
2023-02-22 20:14:37 +0800

    springboot init

---
+++ class-backend-user/src/main/java/com/sunyinuo/classbackenduser/ClassBackendUserApplication.java
@@ -0,0 +1,18 @@
+package com.sunyinuo.classbackenduser;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author martin
+ */
+@SpringBootApplication
+@Slf4j
+public class ClassBackendUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassBackendUserApplication.class, args);
+    }
+
+}
